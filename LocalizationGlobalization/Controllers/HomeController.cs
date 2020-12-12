5c6d4f16c98f5b449bc67618356490fc8e70a067
saitguzel <saitguzel90@hotmail.com>
2020-12-12 19:19:21 +0300

    Add project files.

---
+++ LocalizationGlobalization/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using LocalizationGlobalization.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Localization;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LocalizationGlobalization.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IHtmlLocalizer<HomeController> _localizer;
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger, IHtmlLocalizer<HomeController> localizer)
+        {
+            _logger = logger;
+            _localizer = localizer;
+        }
+
+        public IActionResult Index()
+        {
+            ViewData["Welcome"] = _localizer["Welcome"];
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CultureManagement(string culture,string returnUrl)
+        {
+            Response.Cookies.Append(CookieRequestCultureProvider.DefaultCookieName, CookieRequestCultureProvider.MakeCookieValue(new RequestCulture(culture)), new CookieOptions
+            {
+                Expires = DateTimeOffset.Now.AddDays(30)
+            });
+            return LocalRedirect(returnUrl);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
